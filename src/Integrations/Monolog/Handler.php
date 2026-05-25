69cdf0d79111ad2e8a3a85744d7c00779c33c6d1
Martin Lipskis <lipskis@users.noreply.github.com>
2026-05-25 18:48:06 +0300

    Add `appsignal.group` to $logRecord attribute
    Return a LoggerProxy class from `getLogger` method of
    OpenTelemetry\Contrib\Logs\Monolog\Handler, which allows to inject
    Monolog $logRecord->channel as an `appsignal.group` log attribute.

--- src/Integrations/Monolog/Handler.php
+++ src/Integrations/Monolog/Handler.php
@@ -3,14 +3,52 @@
 namespace Appsignal\Integrations\Monolog;
 
 use OpenTelemetry\API\Globals;
+use OpenTelemetry\API\Logs as API;
+use OpenTelemetry\Context\ContextInterface;
 use OpenTelemetry\Contrib\Logs\Monolog\Handler as MonologHandler;
 
-class Handler
+class Handler extends MonologHandler
 {
-    public static function withLevel(string $level = 'info'): MonologHandler
+    /** @var array<string, LoggerProxy> */
+    private array $proxies = [];
+
+    public static function withLevel(string $level = 'info'): self
     {
         $loggerProvider = Globals::loggerProvider();
 
-        return new MonologHandler(loggerProvider: $loggerProvider, level: $level);
+        return new self(loggerProvider: $loggerProvider, level: $level);
+    }
+
+    protected function getLogger(string $channel): API\LoggerInterface
+    {
+        if (!array_key_exists($channel, $this->proxies)) {
+            $this->proxies[$channel] = new LoggerProxy(parent::getLogger($channel), $channel);
+        }
+
+        return $this->proxies[$channel];
+    }
+}
+
+class LoggerProxy implements API\LoggerInterface
+{
+    public function __construct(
+        private readonly API\LoggerInterface $logger,
+        private readonly string $channel,
+    ) {}
+
+    public function emit(API\LogRecord $logRecord): void
+    {
+        $logRecord->setAttribute('appsignal.group', $this->channel);
+        $this->logger->emit($logRecord);
+    }
+
+    public function logRecordBuilder(): API\LogRecordBuilderInterface
+    {
+        return $this->logger->logRecordBuilder();
+    }
+
+    public function isEnabled(?ContextInterface $context = null, ?int $severityNumber = null, ?string $eventName = null): bool
+    {
+        return $this->logger->isEnabled($context, $severityNumber, $eventName);
     }
 }
